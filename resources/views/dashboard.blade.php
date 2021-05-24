59edd8c2d9e62753991b480349e684d6062c503f
bruno <sup_prog44@ixcsoft.com.br>
2021-05-24 15:28:23 -0300

    melhoria para dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="col-md-4 offset-md-1">
-        <div class="row" style="padding-top:10px;  background-color: #181818; padding-right: 40px; padding-bottom: 25px; border-radius: 10px; margin-top: 20px">
+        <div class="row" style="padding-top:10px;  background-color: #181818; padding-bottom: 25px; border-radius: 10px; margin-top: 20px">
             <div class="col-md-3 offset-md-1">
                 <div style="padding-top:10px;">
                     <i class="bi bi-unlock-fill" style="font-size: 90px; color: #ffffff;"></i>
@@ -24,22 +24,25 @@
                 </div>
             </div>
         </div>
-
     </div>
     <div class="col-md-4 offset-md-2">
-        <div class="row " style="padding-top:10px;  background-color: #181818; padding-right: 40px; padding-bottom: 25px; border-radius: 10px; margin-top: 20px">
-            <div class="col" style="padding-top:10px;">
-                <i class="bi bi-unlock-fill" style="font-size: 90px; color: #ffffff;"></i>
+        <div class="row" style="padding-top:10px;  background-color: #181818; padding-bottom: 25px; border-radius: 10px; margin-top: 20px">
+            <div class="col-md-3 offset-md-1">
+                <div style="padding-top:10px;">
+                    <i class="bi bi-unlock-fill" style="font-size: 90px; color: #ffffff;"></i>
+                </div>
             </div>
-            <div class="col" style="padding-top:10px;">
-                <div class="row justify-content-left" style="padding-top:10px; color: #ffffff; font-size: 35px;">
-                    <label>Decriptografia</label>
-                </div>
-                <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;">
-                    <label>Decifra a senha do ElasticSearch</label>
-                </div>
-                <div class="row justify-content-end" style="padding-left:200px; color: #ffffff;">
-                    <a href="/encdec_elastic" class="btn btn-primary"  style="margin-top: 25px">Prosseguir</a>
+            <div class="col-md-6 offset-md-1">
+                <div style="padding-top: 10px">
+                    <div class="row justify-content-left" style="padding-top:10px; color: #ffffff; font-size: 35px;">
+                        <label>Decriptografia</label>
+                    </div>
+                    <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;">
+                        <label>Decifra a senha do ElasticSearch</label>
+                    </div>
+                    <div class="row justify-content-end" style="padding-left:200px; color: #ffffff;">
+                        <a href="/encdec_elastic" class="btn btn-primary"  style="margin-top: 25px">Prosseguir</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,37 +52,45 @@
 
     @if(Auth::user()->cargo == 'admin')
         <div class="col-md-4 offset-md-1">
-            <div class="row " style="margin-top: 80px; padding-top:10px;  background-color: #181818; padding-right: 40px; padding-bottom: 25px; border-radius: 10px;">
-                <div class="col" style="padding-top:10px;">
-                    <i class="bi bi-person-plus-fill" style="font-size: 90px; color: #ffffff;"></i>
+            <div class="row" style="margin-top: 80px; padding-top:10px;  background-color: #181818; padding-bottom: 25px; border-radius: 10px;">
+                <div class="col-md-3 offset-md-1">
+                    <div style="padding-top:10px;">
+                        <i class="bi bi-person-plus-fill" style="font-size: 90px; color: #ffffff;"></i>
+                    </div>
                 </div>
-                <div class="col" style="padding-top:10px;">
-                    <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;  font-size: 35px">
-                        <label>Gerenciar usuario</label>
-                    </div>
-                    <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;">
-                        <label>Adiciona, edita ou remove usuarios</label>
-                    </div>
-                    <div class="row justify-content-end" style="padding-left:200px; color: #ffffff;">
-                        <a href="/users/list" class="btn btn-primary"  style="margin-top: 25px">Prosseguir</a>
+                <div class="col-md-6 offset-md-1">
+                    <div style="padding-top: 10px">
+                        <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;  font-size: 35px">
+                            <label>Gerenciar usuario</label>
+                        </div>
+                        <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;">
+                            <label>Adiciona, edita ou remove usuarios</label>
+                        </div>
+                        <div class="row justify-content-end" style="padding-left:200px; color: #ffffff;">
+                            <a href="/users/list" class="btn btn-primary"  style="margin-top: 25px">Prosseguir</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-4 offset-md-2">
-            <div class="row " style="padding-top:10px;  background-color: #181818; padding-right: 40px; padding-bottom: 25px; border-radius: 10px; margin-top: 80px">
-                <div class="col" style="padding-top:10px;">
-                    <i class="bi bi-person-lines-fill" style="font-size: 90px; color: #ffffff;"></i>
+            <div class="row" style="padding-top:10px;  background-color: #181818; padding-bottom: 25px; border-radius: 10px; margin-top: 80px">
+                <div class="col-md-3 offset-md-1">
+                    <div style="padding-top:10px;">
+                        <i class="bi bi-person-lines-fill" style="font-size: 90px; color: #ffffff;"></i>
+                    </div>
                 </div>
-                <div class="col" style="padding-top:10px;">
-                    <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;  font-size: 35px">
-                        <label>Logs de usuario</label>
-                    </div>
-                    <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;">
-                        <label>Historico de consultas dos usuarios</label>
-                    </div>
-                    <div class="row justify-content-end" style="padding-left:200px; color: #ffffff;">
-                        <a href="/logs/list" class="btn btn-primary"  style="margin-top: 25px">Prosseguir</a>
+                <div class="col-md-6 offset-md-1">
+                    <div style="padding-top:10px;">
+                        <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;  font-size: 35px">
+                            <label>Logs de usuario</label>
+                        </div>
+                        <div class="row justify-content-left" style="padding-top:10px; color: #ffffff;">
+                            <label>Historico de consultas dos usuarios</label>
+                        </div>
+                        <div class="row justify-content-end" style="padding-left:200px; color: #ffffff;">
+                            <a href="/logs/list" class="btn btn-primary"  style="margin-top: 25px">Prosseguir</a>
+                        </div>
                     </div>
                 </div>
             </div>
